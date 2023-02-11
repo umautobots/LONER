b7637616d47b63ff7a0da84b57ab839f16f58d0b
pckung <pckung@umich.edu>
2023-02-11 14:16:49 -0500

    Revert "Added single-threaded version"
    This reverts commit 52d154baddec4850c4de64f38230d16ff5dc8c67.

--- src/mapping/mapper.py
+++ src/mapping/mapper.py
@@ -45,13 +45,6 @@ class Mapper:
 
         self._term_signal = mp.Value('i', 0)
         self._processed_stop_signal = mp.Value('i', 0)
-<<<<<<< HEAD
-        self.mapping_cnt = 0
-=======
->>>>>>> parent of 52d154b (Added single-threaded version)
-
-    ## Spins by reading frames from the @m frame_slot as inputs.
-    def run(self) -> None:
 
     ## Spins by reading frames from the @m frame_slot as inputs.
     def run(self) -> None:
